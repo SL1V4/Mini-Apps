638e538129ac5548c20f8d383691bf83ea6ee165
Slava Simonenko <slava.simonenko.01@gmail.com>
2024-06-15 18:39:23 +0900

    mini-app#2 update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: false,
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `${process.env.SERVER_URL}/:path*`
+			}
+		]
+	},
+	images: {
+		domains: ['drive.google.com', 'getfile.dokpub.com']
+	}
+}
 
 export default nextConfig
